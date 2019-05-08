7c7a8ce31c555744e2f10995db310f749ca18296
AlenaMaroz <black.panther.92.hu@gmail.com>
2019-05-08 21:13:56 +0300

    + ReviewServiceImpl

---
+++ online-market/service-module/src/main/java/com/gmail/marozalena/onlinemarket/service/exception/ReviewNotFoundException.java
@@ -0,0 +1,8 @@
+package com.gmail.marozalena.onlinemarket.service.exception;
+
+public class ReviewNotFoundException extends RuntimeException {
+
+    public ReviewNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- online-market/service-module/src/main/java/com/gmail/marozalena/onlinemarket/service/impl/ReviewServiceImpl.java
+++ online-market/service-module/src/main/java/com/gmail/marozalena/onlinemarket/service/impl/ReviewServiceImpl.java
@@ -1,16 +1,54 @@
 package com.gmail.marozalena.onlinemarket.service.impl;
 
+import com.gmail.marozalena.onlinemarket.repository.ReviewRepository;
+import com.gmail.marozalena.onlinemarket.repository.model.Review;
 import com.gmail.marozalena.onlinemarket.service.ReviewService;
+import com.gmail.marozalena.onlinemarket.service.converter.ReviewConverter;
+import com.gmail.marozalena.onlinemarket.service.exception.ReviewNotFoundException;
 import com.gmail.marozalena.onlinemarket.service.model.ReviewDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
 
+    private static final Logger logger = LoggerFactory.getLogger(ReviewServiceImpl.class);
+
+    private final ReviewRepository reviewRepository;
+    private final ReviewConverter reviewConverter;
+
+    @Autowired
+    public ReviewServiceImpl(ReviewRepository reviewRepository, ReviewConverter reviewConverter) {
+        this.reviewRepository = reviewRepository;
+        this.reviewConverter = reviewConverter;
+    }
+
     @Override
     public List<ReviewDTO> getReviews() {
-        return null;
+        try(Connection connection = reviewRepository.getConnection()){
+            connection.setAutoCommit(false);
+            try{
+                List<Review> reviews = reviewRepository.getReviews(connection);
+                List<ReviewDTO> list = reviews.stream()
+                        .map(reviewConverter::toReviewDTO)
+                        .collect(Collectors.toList());
+                connection.commit();
+                return list;
+            }catch (Exception e){
+                connection.rollback();
+                logger.error(e.getMessage(), e);
+                throw new ReviewNotFoundException("Reviews not found in database", e);
+            }
+        } catch (SQLException e) {
+            logger.error(e.getMessage(), e);
+            throw new ReviewNotFoundException("Reviews not found in database", e);
+        }
     }
 }
